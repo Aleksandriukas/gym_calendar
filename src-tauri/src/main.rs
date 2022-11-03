7020db1a73d5405498e90e5dcd3297307371787a
Aleksandriukas <aleksandriukas777@gmail.com>
2022-11-03 21:47:30 +0200

    Created struct and components for day

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,7 +9,19 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+struct gym_day {
+    date: String,
+    from: String,
+    to: String,
+    exercises: Vec<String>,
+}
+
+
+
+
 fn main() {
+
+   
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
---
+++ src/components/DayComponent.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./DayComponent.css";
+export type DayComponentProps = {
+  onPress?: () => void;
+  date: Date;
+};
+
+export const DayComponent = ({ date }: DayComponentProps) => {
+  const displayDate = `${date.getFullYear()}/${date.getMonth()}/${date.getDay()}`;
+
+  return (
+    <div>
+      <button>
+        <div className="DayContainer">
+          <p>{displayDate}</p>
+        </div>
+      </button>
+    </div>
+  );
+};
