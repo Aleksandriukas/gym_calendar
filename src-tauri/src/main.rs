5cb78c96f6ada62bd666f1c07d77cba8a90cb7ad
Aleksandriukas <aleksandriukas777@gmail.com>
2022-11-11 20:55:28 +0200

    path_exists() and create_directory()

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,25 +3,32 @@
     windows_subsystem = "macos"
 )]
 
+use std::{fs};
+
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-struct gym_day {
-    date: String,
-    from: String,
-    to: String,
-    exercises: Vec<String>,
+
+fn createDirectory(){
+    fs::create_dir(MAIN_DIRECTORY_NAME);
+}
+
+pub fn path_exists(path: &str) -> bool {
+    fs::metadata(path).is_ok()
 }
 
 
-
-
+static MAIN_DIRECTORY_NAME: &str = "week_plans";
 fn main() {
-
-   
+    
+    if(!path_exists(MAIN_DIRECTORY_NAME)){
+        createDirectory();
+    }
+  
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,14 @@ function App() {
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
 
-  async function greet() {
+  async function test() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
+    await invoke("test");
   }
 
   return (
     <div className="container">
+      <button onClick={test}>Generate file</button>
       <Calendar />
     </div>
   );
